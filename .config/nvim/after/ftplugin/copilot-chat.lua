ca0a019a4f1bb2f13248f4416882617b14eb1fdf
Fernando Caldera <fer.caldera@gmail.com>
2026-02-09 17:29:30 +0200

    nvim: config clean up

--- .config/nvim/after/ftplugin/copilot-chat.lua
+++
@@ -1,9 +0,0 @@
-local set = vim.opt_local
-
-set.wrap = true
-set.textwidth = 80
-set.colorcolumn = "81"
-
--- Allow j/k when navigating wrapped lines
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
--- .config/nvim/queries/embedded_template/injections.scm
+++
@@ -1,5 +0,0 @@
-; extends
-
-((code) @injection.content
-  (#set! injection.language "javascript")
-  (#set! injection.combined))
