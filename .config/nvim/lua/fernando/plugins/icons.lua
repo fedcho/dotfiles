538227b907106378e21377bcaea7318e957f68c4
Fernando Caldera <fer.caldera@gmail.com>
2026-02-09 22:52:33 +0200

    nvim: use old css icon
    while the new is added to my patched font

--- .config/nvim/lua/fernando/plugins/icons.lua
+++ .config/nvim/lua/fernando/plugins/icons.lua
@@ -1,6 +1,14 @@
 return {
   "nvim-tree/nvim-web-devicons",
   opts = {
+    override = {
+      css = {
+        icon = "",
+        color = "#42A5F5",
+        cterm_color = "75",
+        name = "Css",
+      },
+    },
     override_by_filename = {
       [".env.local"] = {
         icon = "",
