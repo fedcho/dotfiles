92d3e6fb74057be771d455b4befa1b03c3cdc017
Fernando Caldera <fer.caldera@gmail.com>
2026-04-08 16:50:27 +0300

    nvim: dabod keymaps

--- .config/nvim/lua/fernando/plugins/dadbod.lua
+++ .config/nvim/lua/fernando/plugins/dadbod.lua
@@ -11,8 +11,17 @@ return {
     "DBUIFindBuffer",
   },
   init = function()
-    -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_execute_on_save = false
+    vim.g.db_ui_auto_execute_table_helpers = 1
+
+    -- Custom Keymaps for SQL buffers
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "sql", "mysql", "plsql" },
+      callback = function()
+        vim.keymap.set({ "n", "v" }, "<leader>ks", "<Plug>(DBUI_ExecuteQuery)", { buffer = true, desc = "Execute Query" })
+        vim.keymap.set({ "n", "v" }, "<leader>kw", "<Plug>(DBUI_SaveQuery)", { buffer = true, desc = "Save Query" })
+      end,
+    })
   end,
 }
