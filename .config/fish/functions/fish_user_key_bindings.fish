c4fb6710c2518865f3239192c48a4b1af844aeef
Fernando Caldera <fer.caldera@gmail.com>
2026-04-11 01:19:45 +0300

    fish: fix pure prompt reverse indicator
    The user key bindings were interfering with the correct behavior of the
    pure prompt for the reverse indicator on not insert modes.

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,8 @@
 function fish_user_key_bindings
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
+    fish_vi_key_bindings 
 
     bind -M fish_vi_command \$ end-of-line
     bind -M insert \cp up-or-search
     bind -M insert \cn down-or-search
+    bind -M insert \cf accept-autosuggestion
 end
