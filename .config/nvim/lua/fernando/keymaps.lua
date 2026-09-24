2ea1026a726a7acbfc0b4558fd88a39a8b05004e
Fernando Caldera <fer.caldera@gmail.com>
2026-09-24 11:32:09 +0300

    nvim: disable custom resize due to aerospace clashes

--- .config/nvim/lua/fernando/keymaps.lua
+++ .config/nvim/lua/fernando/keymaps.lua
@@ -14,8 +14,9 @@ set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Control the size of splits (height/width)
-set("n", "<M-,>", "<c-w>5<")
-set("n", "<M-.>", "<c-w>5>")
+-- Disabled: conflicts with Aerospace
+-- set("n", "<M-,>", "<c-w>5<")
+-- set("n", "<M-.>", "<c-w>5>")
 
 -- Keep the cursor in the middle when jumping vertically
 set("n", "<C-d>", "<C-d>zz")
